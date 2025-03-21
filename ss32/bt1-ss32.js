4568bfcbe5b64f67e94d20c372f1a00a84108981
Phanh2910 <phanphuocanh97@gmail.com>
2025-03-21 10:44:38 +0700

    Update bt1-ss32.js

--- ss32/bt1-ss32.js
+++ ss32/bt1-ss32.js
@@ -1,4 +1,4 @@
-function changeBackgroundColor() {
+function Backgroundcolor() {
     let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     document.getElementById("container").style.backgroundColor = randomColor;
 }
